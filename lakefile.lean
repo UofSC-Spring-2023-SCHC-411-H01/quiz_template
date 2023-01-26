eaf0bbb6fcdd29589cba00bd1a1a48100bd471c9
Matthew Ballard <matt@mrb.email>
2023-01-26 12:26:07 -0500

    add devcontainer and github files

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package «template» {
+  -- add package configuration options here
+}
+
+@[default_target]
+lean_lib «Template» {
+  -- add library configuration options here
+}
